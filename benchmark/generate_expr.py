4290d959c3fe04cfa3bce4c6eeeb8bd326b85b31
Moritz Sanft <moritz.sanft@outlook.de>
2025-01-19 17:21:39 +0100

    benchamark: add exprgen script

---
+++ benchmark/generate_expr.py
@@ -0,0 +1,53 @@
+import random, argparse
+
+"""
+Generate a CNF expression of form `{ x = false, ... } (x || !y) ...`.
+Variables can, but do not need to be assigned.
+"""
+def generate_cnf_expr(num_vars: int, num_clauses: int, var_range_per_clause: tuple[int, int]) -> str:
+    variables = [f"x{i}" for i in range(num_vars)]
+
+    assignments = []
+    for var in variables:
+        if random.choice([True, False]):
+            assignments.append(f"{var} = {random.choice(["true", "false"])}")
+
+    clauses = []
+    for _ in range(num_clauses):
+        clause = []
+        for _ in range(random.randint(*var_range_per_clause)):
+            var = random.choice(variables)
+            if random.choice([True, False]):
+                var = f"!{var}"
+            clause.append(var)
+        clauses.append(f"({" || ".join(clause)})")
+
+    return f"{{{", ".join(assignments)}}} {" && ".join(clauses)}"
+
+parser = argparse.ArgumentParser(
+    prog='generate_expr',
+    description='Generate CNF expressions to solve with desat',
+)
+
+parser.add_argument(
+    'num_vars',
+    type=int,
+    help='Number of variables in the expression',
+)
+
+parser.add_argument(
+    'num_clauses',
+    type=int,
+    help='Number of clauses in the expression',
+)
+
+parser.add_argument(
+    'num_vars_per_clause_range',
+    type=int,
+    nargs=2,
+    help='Range of variables per clause',
+)
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    print(generate_cnf_expr(args.num_vars, args.num_clauses, args.num_vars_per_clause_range))
